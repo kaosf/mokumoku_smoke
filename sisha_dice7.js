4c196facbc7e0461fec76213790c5fec8aaa68b8
anp163 <141214611+anp163@users.noreply.github.com>
2023-08-12 20:48:58 +0900

    Add files via upload

--- sisha_dice7.js
+++ sisha_dice7.js
@@ -1,58 +1,63 @@
-function loadFlavors(flavorsData) {
-    const flavorsList = flavorsData.map(flavor => flavor.name);
-    return flavorsList;
-}
+// your-javascript-file.js
 
-function loadTags(tagsData) {
-    const tagList = [];
-    tagsData.forEach(tagData => {
-        tagList.push(...tagData.tag);
+// flavors_data.json, tags_data.json, blocklist.jsonはリポジトリ内にアップロード済みと仮定
+fetch('flavors_data.json')
+  .then(response => response.json())
+  .then(flavorsData => {
+    const flavors = loadFlavors(flavorsData);
+    const tags = loadTags(flavorsData);
+    const blocklist = loadBlocklist(flavorsData);
+
+    const numDraw = 4;
+    const uniqueResults = new Set();
+
+    while (uniqueResults.size < numDraw) {
+      const selectedFlavor = [...flavors].sort(() => Math.random() - 0.5).slice(0, 3);
+
+      if (!checkExclusion(selectedFlavor, blocklist)) {
+        uniqueResults.add(selectedFlavor.toString());
+      }
+    }
+
+    const resultsContainer = document.querySelector('.results');
+    resultsContainer.innerHTML = ''; // 一度結果をクリア
+
+    uniqueResults.forEach(result => {
+      const resultArray = result.split(',');
+      const resultElement = document.createElement('div');
+      resultElement.textContent = resultArray.join(', ');
+      resultsContainer.appendChild(resultElement);
     });
-    return tagList;
+  });
+
+function loadFlavors(flavorsData) {
+  const flavorsList = flavorsData.map(flavor => flavor.name);
+  return flavorsList;
 }
 
-function loadBlocklist(blockData) {
-    return blockData;
+function loadTags(flavorsData) {
+  const tagList = [];
+  flavorsData.forEach(flavorData => {
+    tagList.push(...flavorData.tag);
+  });
+  return tagList;
+}
+
+function loadBlocklist(flavorsData) {
+  const blocklist = {};
+  flavorsData.forEach(flavorData => {
+    blocklist[flavorData.name] = flavorData.block;
+  });
+  return blocklist;
 }
 
 function checkExclusion(tag, blocklist) {
-    for (const flavorTag of tag) {
-        if (blocklist.hasOwnProperty(flavorTag)) {
-            if (new Set(tag).has(blocklist[flavorTag])) {
-                return true;
-            }
-        }
+  for (const flavorTag of tag) {
+    if (blocklist.hasOwnProperty(flavorTag)) {
+      if (new Set(tag).has(blocklist[flavorTag])) {
+        return true;
+      }
     }
-    return false;
+  }
+  return false;
 }
-
-const flavorsData = [
-    // JSON形式のデータをここに埋め込む
-];
-
-const tagsData = [
-    // JSON形式のデータをここに埋め込む
-];
-
-const blocklistData = {
-    // JSON形式のデータをここに埋め込む
-};
-
-const flavors = loadFlavors(flavorsData);
-const tags = loadTags(tagsData);
-const blocklist = loadBlocklist(blocklistData);
-
-const numDraw = 4;
-const uniqueResults = new Set();
-
-while (uniqueResults.size < numDraw) {
-    const selectedFlavor = [...flavors].sort(() => Math.random() - 0.5).slice(0, 3);
-
-    if (!checkExclusion(selectedFlavor, blocklist)) {
-        uniqueResults.add(selectedFlavor.toString());
-    }
-}
-
-uniqueResults.forEach(result => {
-    console.log(result.split(','));
-});
